9bce0854410da542a258ccab5a81bb2ea3cefddf
unnask <8060462+unnask@users.noreply.github.com>
2023-10-07 17:33:26 -0600

    parse, add std EnvVar

--- src/dotenv.zig
+++ src/dotenv.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const testing = std.testing;
+const EnvMap = std.process.EnvMap;
 
-const KeySpan = std.ArrayList([]u8);
+const KVPairsSpan = std.ArrayList([]u8);
 
 /// Loads a .env file for reading, returning an ArrayList([]u8) with
 /// one line per element.
@@ -13,55 +14,69 @@ const KeySpan = std.ArrayList([]u8);
 /// @return - errorsets - Allocator.Error, File.OpenError, Writer.Error
 ///         - ArrayList of file lines
 ///
-fn readEnvFile(allocator: Allocator, path: []const u8) !KeySpan {
+fn initKVPairsSpan(allocator: Allocator, path: []const u8) !KVPairsSpan {
     var file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
 
     var buf = std.io.bufferedReader(file.reader());
     var reader = buf.reader();
-    var buffer = KeySpan.init(allocator);
-    var keyspan = std.ArrayList(u8).init(allocator);
-    defer keyspan.deinit();
+    var spans = KVPairsSpan.init(allocator);
+    var kvspan = std.ArrayList(u8).init(allocator);
+    defer kvspan.deinit();
 
     rall: while (true) {
-        reader.streamUntilDelimiter(keyspan.writer(), '\n', null) catch |err| switch (err) {
+        reader.streamUntilDelimiter(kvspan.writer(), '\n', null) catch |err| switch (err) {
             error.EndOfStream => {
                 break :rall;
             },
             else => |e| return e,
         };
 
-        try buffer.append(try keyspan.toOwnedSlice());
+        try spans.append(try kvspan.toOwnedSlice());
     }
 
-    return buffer;
+    return spans;
 }
 
 /// Deinitialize the ArrayList returned from readEnvFile
-fn deinitEnvList(allocator: Allocator, var_list: KeySpan) void {
+fn deinitKVEnvSpan(allocator: Allocator, var_list: KVPairsSpan) void {
     for (var_list.items) |e| {
         allocator.free(e);
     }
     var_list.deinit();
 }
 
+fn parseEnvFile(pairs: KVPairsSpan, env: *EnvMap) void {
+    for (pairs.items) |e| {
+        var stream = std.io.fixedBufferStream(e);
+        var reader = stream.reader();
+
+        try reader.skipUntilDelimiterOrEof('=');
+        var key = e[0..stream.getPos()];
+        var value = e[stream.getPos()..];
+
+        env.put(key, value);
+    }
+}
+
 pub const DefaultConfig = struct {
     path: []const u8 = ".env",
+    override: bool = false,
 };
 
-pub fn load(allocator: Allocator) !void {
+pub fn load(allocator: Allocator) !EnvMap {
     try load_conf(allocator, DefaultConfig{});
 }
 
-pub fn load_conf(allocator: Allocator, comptime config: anytype) !void {
+pub fn load_conf(allocator: Allocator, comptime config: anytype) !EnvMap {
     _ = config;
     _ = allocator;
 }
 
 test "readEnvFile happy path" {
-    const span = try readEnvFile(std.testing.allocator, ".env");
+    const span = try initKVPairsSpan(std.testing.allocator, ".env");
     defer {
-        deinitEnvList(std.testing.allocator, span);
+        deinitKVEnvSpan(std.testing.allocator, span);
     }
 
     //for (span.items) |item| {

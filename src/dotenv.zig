c8b14009b4465aede36bd203cc15b375218892b6
unnask <8060462+unnask@users.noreply.github.com>
2023-10-31 19:05:04 -0600

    add whitespace character list

--- src/dotenv.zig
+++ src/dotenv.zig
@@ -2,7 +2,42 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const EnvMap = std.process.EnvMap;
 
-const whitespace = [_]u8{ ' ', '\t', '\r', '\n', 0x0b, 0x0c };
+const whitespace = [_]u8{
+    0x09, // horizontal tab
+    0x0a, // line feed
+    0x0b, // vertical tab
+    0x0c, // form feed
+    0x0d, // carriage return
+    0x20, // space
+    0x85, // next line
+    0xA0, // no-break space
+};
+
+const whitespace_mb = [_]u8{
+    0xe1, 0x9a, 0x80, // U+1680 ogham space mark
+    0xe1, 0xa0, 0x8e, // U+180E mongolian vowel separator
+    0xe2, 0x80, 0x80, // U+2000 en quad
+    0xe2, 0x80, 0x81, // U+2001 em quad
+    0xe2, 0x80, 0x82, // U+2002 en space
+    0xe2, 0x80, 0x83, // U+2003 em space
+    0xe2, 0x80, 0x84, // U+2004 three-per-em space
+    0xe2, 0x80, 0x85, // U+2005 four-per-em space
+    0xe2, 0x80, 0x86, // U+2006 six-per-em space
+    0xe2, 0x80, 0x87, // U+2007 figure space
+    0xe2, 0x80, 0x88, // U+2008 punctuation space
+    0xe2, 0x80, 0x89, // U+2009 thin space
+    0xe2, 0x80, 0x8a, // U+200A hair space
+    0xe2, 0x80, 0x8b, // U+200B zero width space
+    0xe2, 0x80, 0x8c, // U+200C zero width non-joiner
+    0xe2, 0x80, 0x8d, // U+200D zero width joiner
+    0xe2, 0x80, 0xa8, // U+2028 line separator
+    0xe2, 0x80, 0xa9, // U+2029 paragraph separator
+    0xe2, 0x80, 0xaf, // U+202F narrow no-break space
+    0xe2, 0x81, 0x9f, // U+205F medium mathematical space
+    0xe2, 0x81, 0xa0, // U+2060 word joiner
+    0xe3, 0x80, 0x80, // U+3000 ideographic space
+    0xef, 0xbb, 0xbf, // U+FEFF zero width non-breaking space
+};
 
 fn trim(value: []const u8) []const u8 {
     if (value.len == 0) {
